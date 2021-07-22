213a23d4b71a727bd4ac3b5e89be0dff7d85424b
Herbert <hywenklis@hotmail.com>
2021-07-22 14:36:59 -0300

    trilha java

---
+++ bytebank/src/TestaMetodo.java
@@ -0,0 +1,35 @@
+public class TestaMetodo {
+
+    public static void main(String[] args) {
+
+        Conta conta = new Conta();
+
+        conta.saldo = 100;
+        conta.deposita(50);
+        conta.saca(20);
+
+        System.out.println("Saldo da conta1: R$" + conta.saldo);
+
+        boolean conseguiuRetirar = conta.saca(20);
+
+        if (conseguiuRetirar){
+            System.out.println("Novo valor R$" +conta.saldo);
+        } else {
+            System.out.println("Saldo insuficiente para sacar");
+        }
+
+        Conta conta2 = new Conta();
+
+        conta2.deposita(1000);
+
+        boolean sucessoTransferencia = conta2.transfere(300,conta);
+
+        if (sucessoTransferencia){
+            System.out.println("Transferência com sucesso!");
+        } else {
+            System.out.println("Saldo insuficiente!");
+        }
+        System.out.println("Novo valor da conta2: R$" + conta2.saldo);
+        System.out.println("Novo valor da conta1: R$" + conta.saldo);
+    }
+}
