7e6c7cc99b772c67cef501755fea000e211a847b
Herbert <hywenklis@hotmail.com>
2021-08-05 11:16:18 -0300

    Trilha java

---
+++ bytebank-herdado-conta/src/com/br/bytebank/banco/teste/TesteObject.java
@@ -0,0 +1,21 @@
+package com.br.bytebank.banco.teste;
+
+import com.br.bytebank.banco.modelo.Cliente;
+import com.br.bytebank.banco.modelo.ContaCorrente;
+import com.br.bytebank.banco.modelo.ContaPoupanca;
+
+public class TesteObject {
+
+    public static void main(String[] args) {
+
+        System.out.println("x");
+        System.out.println(3);
+        System.out.println(false);
+
+        ContaCorrente cc = new ContaCorrente(22, 33);
+        Object cp = new ContaPoupanca(33, 22);
+
+        System.out.println(cc.toString());
+        System.out.println();
+    }
+}
