91a313ce80f2ea4f2690a7b89e380e86dbd63527
Herbert <hywenklis@hotmail.com>
2021-08-05 17:22:24 -0300

    Trilha java

---
+++ bytebank-herdado-conta/src/com/br/bytebank/banco/util/Teste.java
@@ -0,0 +1,37 @@
+package com.br.bytebank.banco.util;
+
+import com.br.bytebank.banco.modelo.Conta;
+import com.br.bytebank.banco.modelo.ContaCorrente;
+
+import java.util.ArrayList;
+
+public class Teste {
+
+    public static void main(String[] args) {
+
+        ArrayList lista = new ArrayList();
+
+        Conta cc = new ContaCorrente(22,11);
+        lista.add(cc);
+        Conta cc2 = new ContaCorrente(22,13);
+        lista.add(cc2);
+        Conta cc3= new ContaCorrente(22,16);
+        lista.add(cc3);
+        Conta cc4 = new ContaCorrente(22,14);
+        lista.add(cc4);
+
+        System.out.println("Tamanho: " + lista.size());
+
+        System.out.println(lista.get(0));
+        lista.remove(0);
+        System.out.println("Tamanho: " + lista.size());
+
+        /*for(int i = 0; i < lista.size(); i++) {
+            System.out.println(lista.get(i));
+        }*/
+
+        for(Object conta : lista) {
+            System.out.println(conta);
+        }
+    }
+}
