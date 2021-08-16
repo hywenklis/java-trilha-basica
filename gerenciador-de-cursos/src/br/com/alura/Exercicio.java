e3c75f37a2211bfa5b2566180a7ee27e27b8beb2
Herbert <hywenklis@hotmail.com>
2021-08-16 11:36:20 -0300

    Trilha java

---
+++ gerenciador-de-cursos/src/br/com/alura/Exercicio.java
@@ -0,0 +1,33 @@
+package br.com.alura;
+
+import java.util.ArrayList;
+import java.util.Collections;
+import java.util.List;
+
+public class Exercicio {
+
+    public static void main(String[] args) {
+
+        List<String> cursos = new ArrayList<>();
+        cursos.add("Java");
+        cursos.add("Python");
+        cursos.add("Data Science");
+
+        Collections.sort(cursos);
+
+        cursos.forEach(curso -> {
+            System.out.println(curso);
+        });
+
+        cursos.remove(1);
+
+        System.out.println("-------------------");
+        System.out.println("Depois de remover:");
+        for (String curso : cursos) {
+            System.out.println(curso);
+        }
+
+        System.out.println("------------------");
+        System.out.println("Primeiro curso da lista: " + cursos.get(0));
+    }
+}
