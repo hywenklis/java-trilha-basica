c7fef3b02d0142911e5d5c8a0e960574e30544a2
hywenklis <hywenklis@hotmail.com>
2021-12-13 08:33:23 -0300

    utilizando generics na lista encadeada

--- exercicio-dio/src/listaencadeada/Main.java
+++ exercicio-dio/src/listaencadeada/Main.java
@@ -3,10 +3,10 @@ package listaencadeada;
 public class Main {
 
     public static void main(String[] args) {
-        No no1 = new No("Conteúdo no1");
-        No no2 = new No("Conteúdo no2");
-        No no3 = new No("Conteúdo no3");
-        No no4 = new No("Conteúdo no4");
+        No<String> no1 = new No<>("Conteúdo no1");
+        No<String> no2 = new No<>("Conteúdo no2");
+        No<String> no3 = new No<>("Conteúdo no3");
+        No<String> no4 = new No<>("Conteúdo no4");
 
         no1.setProximoNo(no2);
         no2.setProximoNo(no3);
