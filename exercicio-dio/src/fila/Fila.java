061d279231c032460312d97bb948d7d5d68fccfe
hywenklis <hywenklis@hotmail.com>
2021-12-14 17:59:03 -0300

    conceito de fila

---
+++ exercicio-dio/src/fila/Fila.java
@@ -0,0 +1,75 @@
+package fila;
+
+public class Fila<T> {
+
+    private No<T> refNoEntradaFila;
+
+
+    public Fila() {
+        this.refNoEntradaFila = null;
+    }
+
+    public void enqueue(T obj) {
+        No novoNo = new No(obj);
+        novoNo.setRefNo(refNoEntradaFila);
+        refNoEntradaFila = novoNo;
+    }
+
+    public T first() {
+        if (this.isEmpty()) {
+            No primeiroNo = refNoEntradaFila;
+            while (true) {
+                if (primeiroNo.getRefNo() != null) {
+                    primeiroNo = primeiroNo.getRefNo();
+                } else {
+                    break;
+                }
+            }
+            return (T) primeiroNo.getObject();
+        }
+        return null;
+    }
+
+    public T dequeue() {
+        if (this.isEmpty()) {
+            No primeiroNo = refNoEntradaFila;
+            No noAuxiliar = refNoEntradaFila;
+            while (true) {
+                if (primeiroNo.getRefNo() != null) {
+                    noAuxiliar = primeiroNo;
+                    primeiroNo = primeiroNo.getRefNo();
+                } else {
+                    noAuxiliar.setRefNo(null);
+                    break;
+                }
+            }
+            return (T) primeiroNo.getObject();
+        }
+        return null;
+    }
+
+    public boolean isEmpty() {
+        return refNoEntradaFila != null;
+    }
+
+    @Override
+    public String toString() {
+        StringBuilder stringRetorno = new StringBuilder();
+        No noAuxiliar = refNoEntradaFila;
+
+        if (refNoEntradaFila != null) {
+            while (true) {
+                stringRetorno.append(" [No{Objeto=").append(noAuxiliar.getObject()).append("}]---->");
+                if (noAuxiliar.getRefNo() != null) {
+                    noAuxiliar = noAuxiliar.getRefNo();
+                } else {
+                    stringRetorno.append("null");
+                    break;
+                }
+            }
+        }else {
+            stringRetorno = new StringBuilder("null");
+        }
+        return stringRetorno.toString();
+    }
+}
