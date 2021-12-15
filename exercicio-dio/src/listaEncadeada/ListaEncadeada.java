3398090533adf1c99b397112ea9174b4d9e261a9
hywenklis <hywenklis@hotmail.com>
2021-12-15 15:19:34 -0300

    conceito de lista encadeada implementando metodo size e isEmpty

---
+++ exercicio-dio/src/listaEncadeada/ListaEncadeada.java
@@ -0,0 +1,32 @@
+package listaEncadeada;
+
+public class ListaEncadeada<T> {
+    
+    No<T> referenciaEntrada;
+    
+    public ListaEncadeada() {
+        this.referenciaEntrada = null;
+    }
+    
+    public boolean isEmpty() {
+        return referenciaEntrada == null ? true : false;
+    }
+
+    public int size() {
+        int tamanhoLista = 0;
+        No<T> referenciaAux = referenciaEntrada;
+        while (true) {
+            if (referenciaAux != null) {
+                tamanhoLista++;
+                if (referenciaAux.getProximoNo() != null) {
+                    referenciaAux = referenciaAux.getProximoNo();
+                } else {
+                    break;
+                }
+            } else {
+                break;
+            }
+        }
+        return tamanhoLista;
+    }
+}
